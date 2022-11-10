12f1b55436eee3888ebb48284455b370c68c0333
Luis Sequera <luissequeracontreras@gmail.com>
2022-11-10 12:10:36 -0500

    last update files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-/* import './App.css'; */
+import react from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import { CreateTodoButtom } from "./CreateTodoButtom.js";
+//import './App.css';
+
+const todos=[
+  {text:'Cortar cebolla', completed:false},
+  {text:'Tormar el curso de intro a react', completed:false},
+  {text:'Llorar con la llorona', completed:false}
+];
+
 
 function App() {
   return (
-
+    <react.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo =>(
+          <TodoItem key={todo.text} text={todo.text} />))}
+      </TodoList>
+      <CreateTodoButtom />
+    </react.Fragment>
   );
 }
 
---
+++ src/CreateTodoButtom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CreateTodoButtom.css';
+
+function CreateTodoButtom(props) {
+  return (
+    <button className="CreateTodoButtom">+</button>
+  );
+}
+
+export { CreateTodoButtom };
